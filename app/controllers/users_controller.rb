3a2af088b0772616d6d0d31c25cd4942d9a52b16
Jules Gorce <jules.gorce@gmail.com>
2022-03-01 15:01:42 +0100

    spotify config

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,17 @@
 class UsersController < ApplicationController
-  # def authentification
-  #   @user = User.new
-  #   authorize @user
-  # end
+  skip_before_action :authenticate_user!
+
+  def authentication
+
+    skip_authorization
+    redirect_to "#{ENV['SPOTIFY_AUTH_URL']}?client_id=#{ENV['SPOTIFY_CLIENT_ID']}&redirect_uri=#{Rails.env.production? ? ENV['SPOTIFY_AUTH_CALLBACK_PROD'] : ENV['SPOTIFY_AUTH_CALLBACK_DEV']}&scope=user-read-private%20user-read-email&response_type=token&state=123"
+  end
 
   def spotify
     @spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
     # Now you can access user's private data, create playlists and much more
     authorize @spotify_user
-    @spotify_user.country
+    @spotify_user.email
+    raise
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable
 
   has_many :followers, through: :subscriptions
   has_many :followings, through: :subscriptions
