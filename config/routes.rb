f6512037c337cd4a09b288d61fb07214d41495dd
thomas compeyron <compeyron.thomas@live.com>
2022-03-01 14:05:41 +0100

    spotify

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get '/auth/spotify/callback', to: 'users#spotify'
+  get 'search', to: 'pages#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :songs, only: %i[new create]
   resources :playlists do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_28_142301) do
+ActiveRecord::Schema.define(version: 2022_03_01_094714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2022_02_28_142301) do
     t.integer "length"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "spotify_track_id"
   end
 
   create_table "subscriptions", force: :cascade do |t|
