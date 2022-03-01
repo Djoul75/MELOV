1965c6f936abbc517405e84b9ff06979fbd5b6ae
Kevin Leblond <leblond_kevin@hotmail.fr>
2022-03-01 11:47:19 +0100

    Publication controller start + authentication

---
+++ app/controllers/publications_controller.rb
@@ -0,0 +1,42 @@
+class PublicationsController < ApplicationController
+  before_action :publication_find, only: %i[show edit update destroy]
+
+  def index
+    @publications = Publication.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @publication = Publication.new
+  end
+
+  def edit
+  end
+
+  def create
+    @publication = Publication.new(publication_params)
+    if @publication.update(publication_params)
+      redirect_to publication_path(@publication)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @publication.destroy
+  end
+end
+
+
+private
+
+  def publication_find
+    @forest = Forest.find(params[:id])
+  end
+
+  def publication_params
+    params.require(:publication).permit(:content)
+  end
