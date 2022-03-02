dfe2dbc52afbaa7449a8fa30f1ebafd99c5e7178
Kevin Leblond <leblond_kevin@hotmail.fr>
2022-03-02 17:52:15 +0100

    Comment lines to avoid crash

--- app/javascript/controllers/feed_subscription_controller.js
+++ app/javascript/controllers/feed_subscription_controller.js
@@ -5,23 +5,22 @@ export default class extends Controller {
   static values = { feedId: Number }
   static targets = ["messages", "form"]
 
-  connect() {
-    console.log(`Subscribe to the feed with the id ${this.feedIdValue}.`)
-    this.channel = consumer.subscriptions.create(
-      { channel: "FeedChannel", id: this.feedIdValue },
-      { received: data => this.#insertMessageScrollDownAndResetForm(data) },
-      // { received: data => this.messagesTarget.insertAdjacentHTML("afterbegin", data) }
+  // connect() {
+  //   console.log(`Subscribe to the feed with the id ${this.feedIdValue}.`)
+  //   this.channel = consumer.subscriptions.create(
+  //     { channel: "FeedChannel", id: this.feedIdValue },
+  //     // { received: data => this.messagesTarget.insertAdjacentHTML("afterbegin", data) },
+  //     // { received: data => this.messagesTarget.#insertMessageScrollDownAndResetForm(data)},
+  //   }
 
-      // #insertMessageScrollDownAndResetForm(data) {
-      //   this.messagesTarget.insertAdjacentHTML("beforeend", data)
-      //   this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
-      //   this.formTarget.reset()
-      // }
-    )
-  }
+  //   disconnect() {
+  //     console.log("Unsubscribed from the chatroom")
+  //     this.channel.unsubscribe()
+  //   }
 
-  disconnect() {
-    console.log("Unsubscribed from the chatroom")
-    this.channel.unsubscribe()
+    // #insertMessageScrollDownAndResetForm(data) {
+    //   this.messagesTarget.insertAdjacentHTML("afterbegin", data)
+    //   this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+    //   this.formTarget.reset()
+    // }
   }
-}
