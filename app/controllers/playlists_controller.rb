6df5b9ce41a2d1e4265e8708f59c0d5975774870
Chloe Roux <chloe.roux.lorieux@gmail.com>
2022-03-02 21:05:14 +0100

    genres ok

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -13,15 +13,26 @@ class PlaylistsController < ApplicationController
       @playlist.save ? playlist_id = @playlist.id : playlist_id = Playlist.find_by(name: @playlist.name).id
 
       p.tracks.each do |t|
-        song = Song.new(
-          artist: t.artists.first.name,
-          genre: t.album.genres.first,
-          length: t.duration_ms,
-          title: t.name
-        )
-        song.save ? song_id = song.id : song_id = Song.find_by(title: song.title)
+        song = Song.find_by(title: t.name)
+        artist = RSpotify::Artist.find(t.artists.first.id)
 
-        playlist_song = PlaylistSong.new(song_id: song_id, playlist_id: playlist_id)
+        if song
+          song.update!(
+            artist: artist.name,
+            genres: artist.genres,
+            length: t.duration_ms,
+            title: t.name
+          )
+        else
+          song = Song.create!(
+            artist: artist.name,
+            genres: artist.genres,
+            length: t.duration_ms,
+            title: t.name
+          )
+        end
+
+        playlist_song = PlaylistSong.new(song_id: song.id, playlist_id: playlist_id)
 
         playlist_song.save
       end
