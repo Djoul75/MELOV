40389cbdaf1ff585bb592bb2869c0b3adcaf8966
Jules Gorce <jules.gorce@gmail.com>
2022-03-03 11:13:59 +0100

    default profil picture fixed & refresh token removed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :lol
   include Pundit::Authorization
 
   # Pundit: white-list approach.
-  def lol
-    # binding.pry
-  end
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
