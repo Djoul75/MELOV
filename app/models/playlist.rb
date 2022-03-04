7fb1936b9bd18473a1b7e946ae2819253d44a55f
Jules Gorce <jules.gorce@gmail.com>
2022-03-04 14:08:06 +0100

    playlist page avancement v1

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -4,5 +4,12 @@ class Playlist < ApplicationRecord
   has_many :playlist_songs, dependent: :destroy
   has_many :songs, through: :playlist_songs
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
+
+  def spotify_playlist
+    @spotify_playlist ||= begin
+      user = User.find(user_id).spotify_id
+      RSpotify::Playlist.find(user, spotify_id)
+    end
+  end
 end
