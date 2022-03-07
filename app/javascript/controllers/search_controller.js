99995c587dd00e0dca78d62c8fd8be4fa682c767
Kevin Leblond <leblond_kevin@hotmail.fr>
2022-03-07 16:03:29 +0100

    OK Search - Visu à retravailler

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -2,19 +2,29 @@ import { Controller } from "@hotwired/stimulus"
 import consumer from "../channels/consumer"
 
 export default class extends Controller {
-  static targets = ["spotifyurl", "trackid"]
+  static targets = ["trackid", "form", "list", "searchInput"]
 
   connect() {
-    console.log(`coucou du search controller`)
+    // console.log(`coucou du search controller`)
   }
 
   songchoice(e) {
     // document.getElementById('publication_spotify_url').value = ''
-    document.getElementById('publication_spotify_url').value = e.currentTarget.innerHTML.match(/>(.*)<\/p>/)[1];
-    document.getElementById('query').value = e.currentTarget.innerText
+    document.getElementById('publication_spotify_url').value = e.currentTarget.dataset.spotifyId;
+    document.getElementById('query').value = e.currentTarget.innerText;
+    this.listTarget.innerHTML = "";
   }
 
   initsearch() {
     window.history.pushState({}, document.title, "/" + "");
+  }
+
+  update() {
+    const url = `${this.formTarget.action}?query=${this.searchInputTarget.value}`
+    fetch(url, { headers: { 'Accept': 'text/plain' } })
+      .then(response => response.text())
+      .then((data) => {
+        this.listTarget.innerHTML = data;
+      })
   }
 }
