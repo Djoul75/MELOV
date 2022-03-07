550a6c6582a57bdd0be464f9f79b6251ef368313
Jules Gorce <jules.gorce@gmail.com>
2022-03-07 18:13:08 +0100

    fix

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -5,6 +5,7 @@ class PublicationsController < ApplicationController
     # @publications = policy_scope(Publication).where(user_id: following_ids).order(created_at: :desc)
     following_ids = current_user.followings.ids + [current_user.id]
     @publications = policy_scope(Publication).where.not(user_id: following_ids).order(created_at: :desc)
+    @publication = Publication.new
   end
 
   def show
