1a34999021c2f51fe263ce76deb46c8e8577ec0c
Jules Gorce <jules.gorce@gmail.com>
2022-03-07 15:09:54 +0100

    song creee a chaque post pour plus de robustesse

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_07_101835) do
+ActiveRecord::Schema.define(version: 2022_03_07_135138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 2022_03_07_101835) do
     t.string "cover_url"
     t.string "title"
     t.string "artist"
+    t.integer "song_duration"
     t.index ["user_id"], name: "index_publications_on_user_id"
   end
 
